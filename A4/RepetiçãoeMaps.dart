10c8e912a540dccae8f97456c6826fd6879135ca
biia_santosss_ <133895171+BiaSantos01@users.noreply.github.com>
2024-02-28 13:33:57 -0300

    Add files via upload

---
+++ A4/RepetiçãoeMaps.dart
@@ -0,0 +1,13 @@
+void main() {
+  Map<String, double> frutas = {
+    'banana': 2.5,
+    'maçã': 3.0,
+    'laranja': 2.0,
+    'uva': 4.5,
+    'morango': 5.0
+  };
+  print('Lista de frutas e preços:');
+  frutas.forEach((fruta, preco) {
+    print('$fruta - Preço: R\$ $preco');
+  });
+}
